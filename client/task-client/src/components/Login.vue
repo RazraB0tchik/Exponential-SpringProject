363ba952428abc3b2f2a6a0ae98f13b1210a061c
RazraB0tchik <100565729+RazraB0tchik@users.noreply.github.com>
2023-04-13 21:59:31 +1000

    comitTwo

--- client/task-client/src/components/Login.vue
+++ client/task-client/src/components/Login.vue
@@ -1,21 +1,28 @@
 <template>
 
-  <div class="container-fluid" style="background-color: blue">
-    <div class="row" style="background-color: orange; justify-content: center">
-      <div class="col-md-10 col-xl-6 " style="height: 100vh; background-color: palegreen">
-        <div class="d-flex justify-content-center align-items-center" style="background-color: blueviolet; width: 100%; height: 100%">
+  <div class="container-fluid">
+    <div class="row" style="background-color: #4A4A4A; justify-content: center">
+      <div class="col-md-10 col-xl-6 " style="height: 100vh">
+        <div class="d-flex justify-content-center align-items-center" style="width: 100%; height: 100%">
           <div class="login_form d-flex flex-column align-items-center justify-content-around">
             <div class="login_icon">
             </div>
-            <div class="inputs_element d-flex flex-column justify-content-around">
-              <form class="w-100 h-50 d-flex flex-column align-content-around justify-content-around" style="background-color: orange">
+            <div class="inputs_element d-flex flex-column align-items-center justify-content-around">
+              <div class="form-header mb-2">Login</div>
+              <form class="w-100 d-flex flex-column justify-content-around" style="height: 75%">
                 <div class="form-group align-items-center">
-                  <label class="label_text" for="email_form">Введите почту</label>
+                  <label class="label_text text-lg-center text-sm-center" for="email_form">Введите почту</label>
                   <input class = "form-control" type="email" id="email_form">
                 </div>
                 <div class="form-group align-items-center">
-                  <label class="label_text" for="password_form">Введите пароль</label>
+                  <label class="label_text text-lg-center text-sm-center" for="password_form">Введите пароль</label>
                   <input class = "form-control" type="password" id="password_form">
+                </div>
+                <div class="form-group align-items-center">
+                <button type="submit" class="btn">Войти</button>
+                </div>
+                <div class="form-group align-items-center">
+                  <div class="forgot_pass text-lg-center text-sm-center">Забыли пароль</div>
                 </div>
               </form>
             </div>
@@ -39,27 +46,13 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Dancing+Script&family=IM+Fell+Double+Pica:ital@1&family=Open+Sans&family=Oswald&family=PT+Serif:ital@1&family=Raleway:wght@500&display=swap');
-.login_form{
-   background-color: blue;
-   width: 80%;
-   height: 70%;
- }
- .login_icon{
-   background-color: brown;
-   width: 15vh;
-   height: 20%;
-   margin-top: 5%;
-   margin-bottom: 5%;
- }
- .inputs_element{
-   width: 80%;
-   height: 60%;
-   background-color: aqua;
-   margin-bottom: 10%;
- }
- .label_text{
-   font-size: 2vh;
-   font-family: "Da";
- }
+@import "../fonts/fonts.css";
+
+.forgot_pass{
+  color: #FF0DFF;
+  font-family: "Base", sans-serif;
+  cursor: pointer;
+
+}
 
 </style>
