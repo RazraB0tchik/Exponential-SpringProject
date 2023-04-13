8e09aec1e2832f5f289319f908b1688ad08de819
RazraB0tchik <100565729+RazraB0tchik@users.noreply.github.com>
2023-04-13 23:33:52 +1000

    comitThree

--- client/task-client/src/components/Login.vue
+++ client/task-client/src/components/Login.vue
@@ -5,8 +5,7 @@
       <div class="col-md-10 col-xl-6 " style="height: 100vh">
         <div class="d-flex justify-content-center align-items-center" style="width: 100%; height: 100%">
           <div class="login_form d-flex flex-column align-items-center justify-content-around">
-            <div class="login_icon">
-            </div>
+            <LoginAnimateElem/>
             <div class="inputs_element d-flex flex-column align-items-center justify-content-around">
               <div class="form-header mb-2">Login</div>
               <form class="w-100 d-flex flex-column justify-content-around" style="height: 75%">
@@ -19,7 +18,7 @@
                   <input class = "form-control" type="password" id="password_form">
                 </div>
                 <div class="form-group align-items-center">
-                <button type="submit" class="btn">Войти</button>
+                <button type="submit" class="button_login">Войти</button>
                 </div>
                 <div class="form-group align-items-center">
                   <div class="forgot_pass text-lg-center text-sm-center">Забыли пароль</div>
@@ -35,11 +34,16 @@
 </template>
 
 <script>
+import LoginAnimateElem from "@/components/LoginAnimateElem";
 
 export default {
   name: "Login",
   beforeCreate() {
     import("../css/customEl.scss");
+    import ("@/components/LoginAnimateElem");
+  },
+  components: {
+    LoginAnimateElem
   }
 }
 </script>
